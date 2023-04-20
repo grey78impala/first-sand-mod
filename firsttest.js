f6823905e22df4782c0a157afc80afd4f7e37296
superTurtle2474 <105331009+superTurtle2474@users.noreply.github.com>
2023-04-19 19:58:08 -0600

    Update firsttest.js

--- firsttest.js
+++ firsttest.js
@@ -7,3 +7,13 @@ elements.test = {
   state: "liquid",
   density: 720,
 }
+// Create a new tool:
+elements.sand_exploder = {
+    color: "#ff0000",
+    tool: function(pixel) {
+        if (pixel.element == "sand") {
+            pixel.element = "explosion"
+        }
+    },
+    category: "tools",
+};
